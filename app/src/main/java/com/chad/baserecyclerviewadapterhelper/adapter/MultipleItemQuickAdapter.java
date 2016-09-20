5695ce057b503042cf4bfac7816d60f65fb76365
linan <530893850@qq.com>
2016-09-20 11:31:52 +0800

    ｏｎｅ

---
+++ app/src/main/java/com/chad/baserecyclerviewadapterhelper/adapter/MultipleItemQuickAdapter.java
@@ -0,0 +1,39 @@
+package com.chad.baserecyclerviewadapterhelper.adapter;
+
+import android.content.Context;
+
+import com.chad.baserecyclerviewadapterhelper.R;
+import com.chad.baserecyclerviewadapterhelper.entity.MultipleItem;
+import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public class MultipleItemQuickAdapter extends BaseMultiItemQuickAdapter<MultipleItem> {
+
+    public MultipleItemQuickAdapter(Context context, List data) {
+        super(context, data);
+        addItmeType(MultipleItem.TEXT, R.layout.item_text_view);
+        addItmeType(MultipleItem.IMG, R.layout.item_image_view);
+        addItmeType(MultipleItem.IMGS, R.layout.item_image_views);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, MultipleItem item) {
+        switch (helper.getItemViewType()) {
+            case MultipleItem.TEXT:
+                helper.setText(R.id.tv, item.getContent());
+                break;
+            case MultipleItem.IMG:
+                // set img data
+                break;
+            case MultipleItem.IMGS:
+                // set imgs data
+                break;
+        }
+    }
+
+}
